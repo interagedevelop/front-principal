7d331cf36dea29a502c09e67460b724071946074
Felipe <74218430+FelipeRochaSiqueira@users.noreply.github.com>
2022-11-16 12:25:09 -0300

    lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonModule} from 'primeng/button';
+import {LibModule} from 'lib'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {ButtonModule} from 'primeng/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    LibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
